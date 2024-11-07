8018865c66245f2002fe446b7d14e60a051a0185
Subhajit Sahu <wolfram77@gmail.com>
2024-11-07 21:27:55 +0530

    :bug: DEBUG

--- inc/hashtableCuda.hxx
+++ inc/hashtableCuda.hxx
@@ -46,7 +46,6 @@ inline bool __device__ hashtableAccumulateAtCudU(K *hk, V *hv, size_t i, K k, V 
 /**
  * Accumulate value to an entry in hashtable [device function].
  * @tparam BLOCK called from a thread block?
- * @tparam HTYPE hashtable type (0: linear, 1: quadratic, 2: double, 3: quadritic + double)
  * @param hk hashtable keys (updated)
  * @param hv hashtable values (updated)
  * @param H capacity of hashtable (prime)
@@ -55,25 +54,17 @@ inline bool __device__ hashtableAccumulateAtCudU(K *hk, V *hv, size_t i, K k, V 
  * @param v value to accumulate
  * @returns whether value was accumulated
  */
-template <bool BLOCK=false, int HTYPE=0, class K, class V>
+template <bool BLOCK=false, class K, class V>
 inline bool __device__ hashtableAccumulateCudU(K *hk, V * hv, size_t H, size_t T, K k, V v) {
-  size_t i = k, di = 1;
-  for (size_t t=0; t<H; ++t) {
+  size_t i = k, di = 1;  // k % T;
+  for (size_t t=0; t<H; ++t, i+=di, di=di*2 + (k % T))
     if (hashtableAccumulateAtCudU<BLOCK>(hk, hv, i % H, k, v)) return true;
-    switch (HTYPE) {
-      case 0: ++i; break;
-      case 1: i += di; di *= 2; break;
-      case 2: i += k % T; break;
-      case 3: i += di; di = di*2 + (k % T); break;
-    }
-  }
   return false;
 }
 
 
 /**
  * Obtain value of an entry in hashtable [device function].
- * @tparam HTYPE hashtable type (0: linear, 1: quadratic, 2: double, 3: quadritic + double)
  * @param hk hashtable keys
  * @param hv hashtable values
  * @param H capacity of hashtable (prime)
@@ -81,18 +72,11 @@ inline bool __device__ hashtableAccumulateCudU(K *hk, V * hv, size_t H, size_t T
  * @param k entry key
  * @returns entry value
  */
-template <int HTYPE=0, class K, class V>
+template <class K, class V>
 inline V __device__ hashtableGetCud(const K *hk, const V *hv, size_t H, size_t T, K k) {
-  size_t i = k, di = 1;
-  for (size_t t=0; t<H; ++t) {
+  size_t i = k, di = 1;  // k % T;
+  for (size_t t=0; t<H; ++t, i+=di, di=di*2 + (k % T))
     if (hk[i % H]==k) return hv[i % H];
-    switch (HTYPE) {
-      case 0: ++i; break;
-      case 1: i += di; di *= 2; break;
-      case 2: i += k % T; break;
-      case 3: i += di; di = di*2 + (k % T); break;
-    }
-  }
   return V();
 }
 
--- inc/rakCuda.hxx
+++ inc/rakCuda.hxx
@@ -1,13 +1,28 @@
 #pragma once
-#include <cstdint>
+#include <tuple>
 #include <vector>
 #include <algorithm>
+#include <cstdint>
 #include "_main.hxx"
+#include "rak.hxx"
 #include "hashtableCuda.hxx"
 
+using std::tuple;
 using std::vector;
 using std::count_if;
 using std::partition;
+
+
+
+
+#pragma region LAUNCH CONFIG
+#ifndef BLOCK_LIMIT_RAK_THREAD_CUDA
+/** Maximum number of threads per block with RAK thread-per-vertex kernel. */
+#define BLOCK_LIMIT_RAK_THREAD_CUDA  32
+/** Maximum number of threads per block with RAK block-per-vertex kernel. */
+#define BLOCK_LIMIT_RAK_BLOCK_CUDA   128
+#endif
+#pragma endregion
 
 
 
@@ -17,7 +32,7 @@ using std::partition;
 /**
  * Initialize communities such that each vertex is its own community [kernel].
  * @param vcom community each vertex belongs to (output)
- * @param NB begin vertex (inclusive)
+ * @param NB begin vertes (inclusive)
  * @param NE end vertex (exclusive)
  */
 template <class K>
@@ -45,12 +60,11 @@ inline void rakInitializeCuW(K *vcom, K NB, K NE) {
 
 
 
-#pragma region CHOOSE COMMUNITY
+#pragma region UPDATE COMMUNITY MEMBERSHIPS
 /**
  * Scan communities connected to a vertex [device function].
  * @tparam SELF include self-loops?
  * @tparam BLOCK called from a thread block?
- * @tparam HTYPE hashtable type (0: linear, 1: quadratic, 2: double, 3: quadritic + double)
  * @param hk hashtable keys (updated)
  * @param hv hashtable values (updated)
  * @param H capacity of hashtable (prime)
@@ -63,7 +77,7 @@ inline void rakInitializeCuW(K *vcom, K NB, K NE) {
  * @param i start index
  * @param DI index stride
  */
-template <bool SELF=false, bool BLOCK=false, int HTYPE=0, class O, class K, class V, class W>
+template <bool SELF=false, bool BLOCK=false, class O, class K, class V, class W>
 inline void __device__ rakScanCommunitiesCudU(K *hk, W *hv, size_t H, size_t T, const O *xoff, const K *xedg, const V *xwei, K u, const K *vcom, size_t i, size_t DI) {
   size_t EO = xoff[u];
   size_t EN = xoff[u+1] - xoff[u];
@@ -72,15 +86,11 @@ inline void __device__ rakScanCommunitiesCudU(K *hk, W *hv, size_t H, size_t T, 
     W w = xwei[EO+i];
     K c = vcom[v];
     if (!SELF && u==v) continue;
-    hashtableAccumulateCudU<BLOCK, HTYPE>(hk, hv, H, T, c+1, w);
+    hashtableAccumulateCudU<BLOCK>(hk, hv, H, T, c+1, w);
   }
 }
-#pragma endregion
 
 
-
-
-#pragma region MOVE ITERATION
 /**
  * Mark out-neighbors of a vertex as affected [device function].
  * @param vaff vertex affected flags (updated)
@@ -96,15 +106,13 @@ inline void __device__ rakMarkNeighborsCudU(F *vaff, const O *xoff, const K *xed
   size_t EN = xoff[u+1] - xoff[u];
   for (; i<EN; i+=DI) {
     K v = xedg[EO+i];
-    vaff[v] = F(1);  // Use two (synchronous) buffers?
+    vaff[v] = F(1);  // TODO: Use two (synchronous) buffers?
   }
 }
 
 
 /**
  * Move each vertex to its best community, using thread-per-vertex approach [kernel].
- * @tparam BLIM maximum number of threads per block
- * @tparam HTYPE hashtable type (0: linear, 1: quadratic, 2: double, 3: quadritic + double)
  * @param ncom number of changed vertices (updated)
  * @param vcom community each vertex belongs to (updated)
  * @param vaff vertex affected flags (updated)
@@ -117,28 +125,29 @@ inline void __device__ rakMarkNeighborsCudU(F *vaff, const O *xoff, const K *xed
  * @param NE end vertex (exclusive)
  * @param PICKLESS allow only picking smaller community id?
  */
-template <int BLIM=32, int HTYPE=0, class O, class K, class V, class W, class F>
+template <class O, class K, class V, class W, class F>
 void __global__ rakMoveIterationThreadCukU(uint64_cu *ncom, K *vcom, F *vaff, K *bufk, W *bufw, const O *xoff, const K *xedg, const V *xwei, K NB, K NE, bool PICKLESS) {
   DEFINE_CUDA(t, b, B, G);
-  __shared__ uint64_cu ncomb[BLIM];
-  // K shrk[2*DMAX];
-  // W shrw[2*DMAX];
+  __shared__ uint64_cu ncomb[BLOCK_LIMIT_RAK_THREAD_CUDA];
+  const int  MAX_DEGREE = BLOCK_LIMIT_RAK_THREAD_CUDA;
+  K shrk[2 * MAX_DEGREE];
+  W shrw[2 * MAX_DEGREE];
   ncomb[t] = 0;
   for (K u=NB+B*b+t; u<NE; u+=G*B) {
     if (!vaff[u]) continue;
     // Scan communities connected to u.
     K d = vcom[u];
-    size_t EO = xoff[u];
+    // size_t EO = xoff[u];
     size_t EN = xoff[u+1] - xoff[u];
     size_t H  = nextPow2Cud(EN) - 1;
     size_t T  = nextPow2Cud(H)  - 1;
-    K *hk = bufk + 2*EO;  // shrk;
-    W *hv = bufw + 2*EO;  // shrw;
+    K *hk = shrk;  // bufk + 2*EO;
+    W *hv = shrw;  // bufw + 2*EO;
     hashtableClearCudW(hk, hv, H, 0, 1);
-    rakScanCommunitiesCudU<false, false, HTYPE>(hk, hv, H, T, xoff, xedg, xwei, u, vcom, 0, 1);
+    rakScanCommunitiesCudU(hk, hv, H, T, xoff, xedg, xwei, u, vcom, 0, 1);
     // Find best community for u.
     hashtableMaxCudU(hk, hv, H, 0, 1);
-    vaff[u] = F(0);         // Mark u as unaffected (Use two buffers?)
+    vaff[u] = F(0);         // Mark u as unaffected (TODO: Use two buffers?)
     if  (!hk[0]) continue;  // No community found
     K c = hk[0] - 1;        // Best community
     if (c==d) continue;
@@ -156,8 +165,6 @@ void __global__ rakMoveIterationThreadCukU(uint64_cu *ncom, K *vcom, F *vaff, K 
 
 /**
  * Move each vertex to its best community, using thread-per-vertex approach.
- * @tparam BLIM maximum number of threads per block
- * @tparam HTYPE hashtable type (0: linear, 1: quadratic, 2: double, 3: quadritic + double)
  * @param ncom number of changed vertices (updated)
  * @param vcom community each vertex belongs to (updated)
  * @param vaff vertex affected flags (updated)
@@ -170,18 +177,16 @@ void __global__ rakMoveIterationThreadCukU(uint64_cu *ncom, K *vcom, F *vaff, K 
  * @param NE end vertex (exclusive)
  * @param PICKLESS allow only picking smaller community id?
  */
-template <int BLIM=32, int HTYPE=0, class O, class K, class V, class W, class F>
+template <class O, class K, class V, class W, class F>
 inline void rakMoveIterationThreadCuU(uint64_cu *ncom, K *vcom, F *vaff, K *bufk, W *bufw, const O *xoff, const K *xedg, const V *xwei, K NB, K NE, bool PICKLESS) {
-  const int B = blockSizeCu(NE-NB, BLIM);
+  const int B = blockSizeCu(NE-NB,   BLOCK_LIMIT_RAK_THREAD_CUDA);
   const int G = gridSizeCu (NE-NB, B, GRID_LIMIT_MAP_CUDA);
-  rakMoveIterationThreadCukU<BLIM, HTYPE><<<G, B>>>(ncom, vcom, vaff, bufk, bufw, xoff, xedg, xwei, NB, NE, PICKLESS);
+  rakMoveIterationThreadCukU<<<G, B>>>(ncom, vcom, vaff, bufk, bufw, xoff, xedg, xwei, NB, NE, PICKLESS);
 }
 
 
 /**
  * Move each vertex to its best community, using block-per-vertex approach [kernel].
- * @tparam BLIM maximum number of threads per block
- * @tparam HTYPE hashtable type (0: linear, 1: quadratic, 2: double, 3: quadritic + double)
  * @param ncom number of changed vertices (updated)
  * @param vcom community each vertex belongs to (updated)
  * @param vaff vertex affected flags (updated)
@@ -194,33 +199,31 @@ inline void rakMoveIterationThreadCuU(uint64_cu *ncom, K *vcom, F *vaff, K *bufk
  * @param NE end vertex (exclusive)
  * @param PICKLESS allow only picking smaller community id?
  */
-template <int BLIM=128, int HTYPE=0, class O, class K, class V, class W, class F>
+template <class O, class K, class V, class W, class F>
 void __global__ rakMoveIterationBlockCukU(uint64_cu *ncom, K *vcom, F *vaff, K *bufk, W *bufw, const O *xoff, const K *xedg, const V *xwei, K NB, K NE, bool PICKLESS) {
   DEFINE_CUDA(t, b, B, G);
+  const int MAX_DEGREE = BLOCK_LIMIT_RAK_BLOCK_CUDA;  // 128
   uint64_cu ncomb = 0;
-  // __shared__ K shrk[2 * MAX_DEGREE];
-  // __shared__ W shrw[2 * MAX_DEGREE];
-  __shared__ bool vaffb;
+  __shared__ K shrk[2 * MAX_DEGREE];
+  __shared__ W shrw[2 * MAX_DEGREE];
   for (K u=NB+b; u<NE; u+=G) {
-    if (t==0) vaffb = vaff[u];
-    __syncthreads();
-    if (!vaffb) continue;
+    if (!vaff[u]) continue;
     // Scan communities connected to u.
     K d = vcom[u];
     size_t EO = xoff[u];
     size_t EN = xoff[u+1] - xoff[u];
     size_t H  = nextPow2Cud(EN) - 1;
     size_t T  = nextPow2Cud(H)  - 1;
-    K *hk = bufk + 2*EO;  // EN <= MAX_DEGREE? shrk : bufk + 2*EO;
-    W *hv = bufw + 2*EO;  // EN <= MAX_DEGREE? shrw : bufw + 2*EO;
+    K *hk = EN <= MAX_DEGREE? shrk : bufk + 2*EO;
+    W *hv = EN <= MAX_DEGREE? shrw : bufw + 2*EO;
     hashtableClearCudW(hk, hv, H, t, B);
     __syncthreads();
-    rakScanCommunitiesCudU<false, true, HTYPE>(hk, hv, H, T, xoff, xedg, xwei, u, vcom, t, B);
+    rakScanCommunitiesCudU<false, true>(hk, hv, H, T, xoff, xedg, xwei, u, vcom, t, B);
     __syncthreads();
     // Find best community for u.
     hashtableMaxCudU<true>(hk, hv, H, t, B);
     __syncthreads();
-    if (t==0) vaff[u] = F(0);  // Mark u as unaffected (Use two buffers?)
+    if (t==0) vaff[u] = F(0);  // Mark u as unaffected (TODO: Use two buffers?)
     if  (!hk[0]) continue;     // No community found
     K c = hk[0] - 1;           // Best community
     if (c==d) continue;
@@ -237,8 +240,6 @@ void __global__ rakMoveIterationBlockCukU(uint64_cu *ncom, K *vcom, F *vaff, K *
 
 /**
  * Move each vertex to its best community, using block-per-vertex approach.
- * @tparam BLIM maximum number of threads per block
- * @tparam HTYPE hashtable type (0: linear, 1: quadratic, 2: double, 3: quadritic + double)
  * @param ncom number of changed vertices (output)
  * @param vcom community each vertex belongs to (updated)
  * @param vaff vertex affected flags (updated)
@@ -251,17 +252,45 @@ void __global__ rakMoveIterationBlockCukU(uint64_cu *ncom, K *vcom, F *vaff, K *
  * @param NE end vertex (exclusive)
  * @param PICKLESS allow only picking smaller community id?
  */
-template <int BLIM=128, int HTYPE=0, class O, class K, class V, class W, class F>
+template <class O, class K, class V, class W, class F>
 inline void rakMoveIterationBlockCuU(uint64_cu *ncom, K *vcom, F *vaff, K *bufk, W *bufw, const O *xoff, const K *xedg, const V *xwei, K NB, K NE, bool PICKLESS) {
-  const int B = blockSizeCu<true>(NE-NB, BLIM);
+  const int B = blockSizeCu<true>(NE-NB,   BLOCK_LIMIT_RAK_BLOCK_CUDA);
   const int G = gridSizeCu <true>(NE-NB, B, GRID_LIMIT_MAP_CUDA);
-  rakMoveIterationBlockCukU<BLIM, HTYPE><<<G, B>>>(ncom, vcom, vaff, bufk, bufw, xoff, xedg, xwei, NB, NE, PICKLESS);
+  rakMoveIterationBlockCukU<<<G, B>>>(ncom, vcom, vaff, bufk, bufw, xoff, xedg, xwei, NB, NE, PICKLESS);
+}
+#pragma endregion
+
+
+
+
+#pragma region CROSS CHECK
+template <class K>
+void __global__ rakCrossCheckCukU(uint64_cu *ncom, K *vcom, K *vdom, K NB, K NE) {
+  DEFINE_CUDA(t, b, B, G);
+  for (K u=NB+B*b+t; u<NE; u+=G*B) {
+    K c = vdom[u];
+    if (vdom[u]==vcom[u]) continue;
+    if (vdom[c]==c) vcom[u] = c;
+    else atomicAdd(ncom, uint64_cu()-1);  // Bad things happened
+  }
 }
 
 
+template <class K>
+inline void rakCrossCheckCuU(uint64_cu *ncom, K *vcom, K *vdom, K NB, K NE) {
+  const int B = blockSizeCu(NE-NB,   BLOCK_LIMIT_MAP_CUDA);
+  const int G = gridSizeCu (NE-NB, B, GRID_LIMIT_MAP_CUDA);
+  rakCrossCheckCukU<<<G, B>>>(ncom, vcom, vdom, NB, NE);
+}
+#pragma endregion
+
+
+
+
+#pragma region COMPUTATION LOOP
 /**
  * Perform RAK iterations.
- * @tparam HTYPE hashtable type (0: linear, 1: quadratic, 2: double, 3: quadritic + double)
+ * @tparam PICKSTEP allow only picking smaller community id, every PICKSTEP iterations?
  * @param ncom number of changed vertices (updated)
  * @param vcom community each vertex belongs to (updated)
  * @param vaff vertex affected flags (updated)
@@ -276,7 +305,7 @@ inline void rakMoveIterationBlockCuU(uint64_cu *ncom, K *vcom, F *vaff, K *bufk,
  * @param L maximum number of iterations [20]
  * @returns number of iterations performed
  */
-template <int HTYPE=0, class O, class K, class V, class W, class F>
+template <class O, class K, class V, class W, class F>
 inline int rakLoopCuU(uint64_cu *ncom, K *vcom, F *vaff, K *bufk, W *bufw, const O *xoff, const K *xedg, const V *xwei, K N, K NL, double E, int L) {
   int l = 0;
   uint64_cu n = 0;
@@ -284,15 +313,20 @@ inline int rakLoopCuU(uint64_cu *ncom, K *vcom, F *vaff, K *bufk, W *bufw, const
   while (l<L) {
     bool PICKLESS = l % PICKSTEP == 0;
     fillValueCuW(ncom, 1, uint64_cu());
-    rakMoveIterationThreadCuU<32, HTYPE>(ncom, vcom, vaff, bufk, bufw, xoff, xedg, xwei, K(), NL, PICKLESS);
-    rakMoveIterationBlockCuU<128, HTYPE>(ncom, vcom, vaff, bufk, bufw, xoff, xedg, xwei, NL,  N,  PICKLESS); ++l;
+    rakMoveIterationThreadCuU(ncom, vcom, vaff, bufk, bufw, xoff, xedg, xwei, K(), NL, PICKLESS);
+    rakMoveIterationBlockCuU (ncom, vcom, vaff, bufk, bufw, xoff, xedg, xwei, NL,  N,  PICKLESS); ++l;
+    // rakCrossCheckCuU(ncom, vdom, vcom, K(), N); swap(vdom, vcom);
     TRY_CUDA( cudaMemcpy(&n, ncom, sizeof(uint64_cu), cudaMemcpyDeviceToHost) );
     if (!PICKLESS && double(n)/N <= E) break;
   }
   return l;
 }
+#pragma endregion
 
 
+
+
+#pragma region PARTITION
 /**
  * Partition vertices into low-degree and high-degree sets.
  * @param ks vertex keys (updated)
@@ -320,53 +354,54 @@ inline size_t rakPartitionVerticesCudaU(vector<K>& ks, const G& x) {
 #pragma region ENVIRONMENT SETUP
 /**
  * Setup and perform the RAK algorithm.
+ * @tparam FLAG flag type for tracking affected vertices
  * @param x original graph
+ * @param q initial community each vertex belongs to
  * @param o rak options
- * @param fi initialzing community membership (vcomD)
- * @param fm marking affected vertices (vaffD)
- * @param fo process output data (vcom, vcomD)
+ * @param fm marking affected vertices / preprocessing to be performed (vaff)
  * @returns rak result
  */
-template <int HTYPE=3, class G, class FI, class FM, class FO>
-inline auto rakInvokeCuda(const G& x, const RakOptions& o, FI fi, FM fm, FO fo) {
-  using K = typename G::key_type;
+template <class FLAG=char, class G, class K, class FM>
+inline RakResult<K> rakInvokeCuda(const G& x, const vector<K>* q, const RakOptions& o, FM fm) {
   using V = typename G::edge_value_type;
   using W = RAK_WEIGHT_TYPE;
-  using O = uint64_cu;
-  using F = char;
-  int L = o.maxIterations, l = 0;
-  int T = omp_get_max_threads();
-  double E = o.tolerance;
-  // Get graph properties.
+  using O = uint32_t;
+  using F = FLAG;
   size_t S = x.span();
   size_t N = x.order();
   size_t M = x.size();
-  // Allocate buffers.
-  int R = reduceSizeCu(N);
-  vector<O> xoff(N+1);  // CSR offsets array
-  vector<K> xedg(M);    // CSR edge keys array
-  vector<V> xwei(M);    // CSR edge values array
-  O *xoffD = nullptr;  // CSR offsets array [device]
-  K *xedgD = nullptr;  // CSR edge keys array [device]
-  V *xweiD = nullptr;  // CSR edge values array [device]
-  F *vaffD = nullptr;  // Affected vertex flag [device]
-  K *vcomD = nullptr;  // Community membership [device]
-  K *bufkD = nullptr;  // Buffer for hashtable keys [device]
-  W *bufwD = nullptr;  // Buffer for hashtable values [device]
-  uint64_cu *ncomD = nullptr;  // Number of changed vertices [device]
+  int    R = reduceSizeCu(N);
+  int    L = o.maxIterations, l = 0;
+  double E = o.tolerance;
+  vector<O> xoff(N+1);
+  vector<K> xedg(M);
+  vector<V> xwei(M);
+  vector<K> vcom(S), vcomc(N);
+  vector<F> vaff(S), vaffc(N);
+  O *xoffD = nullptr;
+  K *xedgD = nullptr;
+  V *xweiD = nullptr;
+  K *vcomD = nullptr;
+  F *vaffD = nullptr;
+  K *bufkD = nullptr;
+  W *bufwD = nullptr;
+  uint64_cu *ncomD = nullptr;
   // Partition vertices into low-degree and high-degree sets.
   vector<K> ks = vertexKeys(x);
   size_t NL = rakPartitionVerticesCudaU(ks, x);
-  // Obtain CSR representation of the graph.
+  // Obtain data for CSR.
   csrCreateOffsetsW (xoff, x, ks);
   csrCreateEdgeKeysW(xedg, x, ks);
   csrCreateEdgeValuesW(xwei, x, ks);
+  // Obtain initial community membership.
+  if (q) gatherValuesW(vcomc, *q, ks);
   // Allocate device memory.
+  TRY_CUDA( cudaSetDeviceFlags(cudaDeviceMapHost) );
   TRY_CUDA( cudaMalloc(&xoffD, (N+1) * sizeof(O)) );
   TRY_CUDA( cudaMalloc(&xedgD,  M    * sizeof(K)) );
   TRY_CUDA( cudaMalloc(&xweiD,  M    * sizeof(V)) );
-  TRY_CUDA( cudaMalloc(&vaffD,  N    * sizeof(F)) );
   TRY_CUDA( cudaMalloc(&vcomD,  N    * sizeof(K)) );
+  TRY_CUDA( cudaMalloc(&vaffD,  N    * sizeof(F)) );
   TRY_CUDA( cudaMalloc(&bufkD, (2*M) * sizeof(K)) );
   TRY_CUDA( cudaMalloc(&bufwD, (2*M) * sizeof(W)) );
   TRY_CUDA( cudaMalloc(&ncomD,  1    * sizeof(uint64_cu)) );
@@ -374,29 +409,32 @@ inline auto rakInvokeCuda(const G& x, const RakOptions& o, FI fi, FM fm, FO fo) 
   TRY_CUDA( cudaMemcpy(xoffD, xoff.data(), (N+1) * sizeof(O), cudaMemcpyHostToDevice) );
   TRY_CUDA( cudaMemcpy(xedgD, xedg.data(),  M    * sizeof(K), cudaMemcpyHostToDevice) );
   TRY_CUDA( cudaMemcpy(xweiD, xwei.data(),  M    * sizeof(V), cudaMemcpyHostToDevice) );
-  // Perform RAK algorithm.
-  float tm = 0, ti = 0;  // Time spent in different phases
-  float t  = measureDuration([&]() {
-    // Initialize community membership [device].
-    ti += measureDuration([&]() { fi(vcomD); });
-    // Mark affected vertices [device].
-    tm += measureDuration([&]() { fm(vaffD); });
-    // Perform iterations.
-    l = rakLoopCuU<HTYPE>(ncomD, vcomD, vaffD, bufkD, bufwD, xoffD, xedgD, xweiD, K(N), K(NL), E, L);
+  // Perform RAK algorithm on device.
+  float tm = 0;
+  float t  = measureDurationMarked([&](auto mark) {
+    // Setup initial community membership.
+    if (q) TRY_CUDA( cudaMemcpy(vcomD, vcomc.data(), N * sizeof(K), cudaMemcpyHostToDevice) );
+    else   rakInitializeCuW(vcomD, K(), K(N));
+    // Mark initial affected vertices.
+    tm += mark([&]() { fm(vaff); });
+    gatherValuesW(vaffc, vaff, ks);
+    TRY_CUDA( cudaMemcpy(vaffD, vaffc.data(), N * sizeof(F), cudaMemcpyHostToDevice) );
+    // Perform RAK iterations.
+    mark([&]() { l = rakLoopCuU(ncomD, vcomD, vaffD, bufkD, bufwD, xoffD, xedgD, xweiD, K(N), K(NL), E, L); });
   }, o.repeat);
-  // Obtain community membership.
-  vector<K> vcom(S);
-  fo(vcom, vcomD);
+  // Obtain final community membership.
+  TRY_CUDA( cudaMemcpy(vcomc.data(), vcomD, N * sizeof(K), cudaMemcpyDeviceToHost) );
+  scatterValuesW(vcom, vcomc, ks);
   // Free device memory.
   TRY_CUDA( cudaFree(xoffD) );
   TRY_CUDA( cudaFree(xedgD) );
   TRY_CUDA( cudaFree(xweiD) );
-  TRY_CUDA( cudaFree(vaffD) );
   TRY_CUDA( cudaFree(vcomD) );
+  TRY_CUDA( cudaFree(vaffD) );
   TRY_CUDA( cudaFree(bufkD) );
   TRY_CUDA( cudaFree(bufwD) );
   TRY_CUDA( cudaFree(ncomD) );
-  return RakResult<K>(vcom, l, t, tm/o.repeat, ti/o.repeat);
+  return {vcom, l, t, tm/o.repeat};
 }
 #pragma endregion
 
@@ -406,25 +444,17 @@ inline auto rakInvokeCuda(const G& x, const RakOptions& o, FI fi, FM fm, FO fo) 
 #pragma region STATIC
 /**
  * Obtain the community membership of each vertex with Static RAK.
- * @tparam HTYPE hashtable type (0: linear, 1: quadratic, 2: double, 3: quadritic + double)
+ * @tparam FLAG flag type for tracking affected vertices
  * @param x original graph
  * @param o rak options
  * @returns rak result
  */
-template <int HTYPE=3, class G>
+template <class FLAG=char, class G>
 inline auto rakStaticCuda(const G& x, const RakOptions& o={}) {
-  using  K = typename G::key_type;
-  using  F = char;
-  size_t N = x.order();
-  vector<K> ks = vertexKeys(x);
-  auto  fi = [&](K *vcomD) { rakInitializeCuW(vcomD, K(), K(N)); };
-  auto  fm = [&](F *vaffD) { fillValueCuW(vaffD, N, F(1)); };
-  auto  fo = [&](auto& vcom, const K *vcomD) {
-    vector<K> vcomc(N);
-    TRY_CUDA( cudaMemcpy(vcomc.data(), vcomD, N * sizeof(K), cudaMemcpyDeviceToHost) );
-    gatherValuesOmpW(vcom, vcomc, ks);
-  };
-  return rakInvokeCuda<HTYPE>(x, o, fi, fm, fo);
+  using K = typename G::key_type;
+  vector<K> *q = nullptr;
+  auto fm = [](auto& vaff) { fillValueOmpU(vaff, FLAG(1)); };
+  return rakInvokeCuda<FLAG>(x, q, o, fm);
 }
 #pragma endregion
 #pragma endregion
